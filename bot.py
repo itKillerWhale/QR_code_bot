5f499faa528487de9da42792fd5c161c3df78d61
itKillerWhale <br3027645@gmail.com>
2023-06-22 16:30:58 +0300

    Все настройки работают

--- bot.py
+++ bot.py
@@ -20,6 +20,8 @@ class CreateQRCode(StatesGroup):
 
 
 class Settings(StatesGroup):
+    menu = State()
+
     size = State()
     qzone = State()
     margin = State()
@@ -42,6 +44,41 @@ def get_keyboard_for_settings() -> InlineKeyboardMarkup:
                                                  InlineKeyboardButton(text="Цвет", callback_data="color"),
                                                  InlineKeyboardButton(text="Цвет фона", callback_data="bgcolor")],
                                                 [InlineKeyboardButton(text="Готово", callback_data="done")]])
+    return ikb
+
+
+def get_keyboard_for_ecc() -> InlineKeyboardMarkup:
+    ikb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Низкий (~7%)", callback_data="L")],
+                                                [InlineKeyboardButton(text="Нормальный (~15%)", callback_data="M")],
+                                                [InlineKeyboardButton(text="Средний (~25%)", callback_data="Q")],
+                                                [InlineKeyboardButton(text="Большой (~30%)", callback_data="H")]])
+    return ikb
+
+
+def get_keyboard_for_color() -> InlineKeyboardMarkup:
+    ikb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Черный", callback_data="0-0-0"),
+                                                 InlineKeyboardButton(text="Белый", callback_data="255-255-255")],
+
+                                                [InlineKeyboardButton(text="Крассный", callback_data="255-0-0"),
+                                                 InlineKeyboardButton(text="Бардовый", callback_data="139-0-0)")],
+
+                                                [InlineKeyboardButton(text="Зелёный", callback_data="0-128-0"),
+                                                 InlineKeyboardButton(text="Лаймовый", callback_data="0-255-0)")],
+
+                                                [InlineKeyboardButton(text="Жёлтый", callback_data="255-255-0"),
+                                                 InlineKeyboardButton(text="Золотой", callback_data="255-215-0")],
+
+                                                [InlineKeyboardButton(text="Голубой", callback_data="0-255-255"),
+                                                 InlineKeyboardButton(text="Аквамарин",
+                                                                      callback_data="127-255-212")],
+
+                                                [InlineKeyboardButton(text="Синий", callback_data="0-0-255"),
+                                                 InlineKeyboardButton(text="Индиго", callback_data="75-0-130")],
+
+                                                [InlineKeyboardButton(text="Фиолетовый", callback_data="128-0-128"),
+                                                 InlineKeyboardButton(text="Светло-фиолетовый",
+                                                                      callback_data="148-0-211")]
+                                                ])
     return ikb
 
 
@@ -75,8 +112,11 @@ async def get_text_for_qr_code(message: types.Message, state: FSMContext) -> Non
 async def settings(message: types.Message, state: FSMContext) -> None:
     async with state.proxy() as data:
         data["menu"] = await message.answer(
-            text="Нажмите на нужный пункт для полной информации и настройки.\nНастройки сохраняються сразу после изменения.",
+            text="Нажмите на нужный пункт для полной информации и настройки."
+                 "\nНастройки сохраняються сразу после изменения.",
             reply_markup=get_keyboard_for_settings())
+    await message.delete()
+    await Settings.menu.set()
 
 
 async def go_to_menu(message: types.Message, state: FSMContext):
@@ -84,28 +124,98 @@ async def go_to_menu(message: types.Message, state: FSMContext):
                                 text="Нажмите на нужный пункт для полной информации и настройки."
                                      "\nНастройки сохраняються сразу после изменения.",
                                 reply_markup=get_keyboard_for_settings())
-    await state.finish()
+    await Settings.menu.set()
 
 
-@dp.callback_query_handler()
+@dp.callback_query_handler(state=Settings.menu)
 async def settings_menu_callback_query(callback: types.CallbackQuery, state: FSMContext):
     if callback.data == "size":
         await bot.edit_message_text(message_id=callback.message.message_id, chat_id=callback.message.chat.id,
-                                    text="Введите размер qr-кода в пикселях. 1 число - значение стороны квадрата. "
-                                         "В диопазоне от 10 до 1000", )
+                                    text="Размер qr-кода в пикселях. Число в диопазоне от 10 до 1000", )
         await Settings.size.set()
+
+    elif callback.data == "margin":
+        await bot.edit_message_text(message_id=callback.message.message_id, chat_id=callback.message.chat.id,
+                                    text="Толщина поля в пикселях. Поля всегда будут иметь тот же цвет, что и фон "
+                                         "(вы можете настроить это через bgcolor ). Он не будет добавляться к ширине"
+                                         " изображения, заданной параметром size , поэтому он должен быть меньше, чем"
+                                         " одна треть значения размера. Число в диопазоне от 0 до 50", )
+        await Settings.margin.set()
+
+    elif callback.data == "qzone":
+        await bot.edit_message_text(message_id=callback.message.message_id, chat_id=callback.message.chat.id,
+                                    text="Толщина поля («тихая зона», область без мешающих элементов, помогающая "
+                                         "читателям найти QR-код) Тихая зона будет нарисована в дополнение к конечному"
+                                         " установленному значению размера. Число в диопазоне от 0 до 100", )
+        await Settings.qzone.set()
+
+    elif callback.data == "ecc":
+        await bot.edit_message_text(message_id=callback.message.message_id, chat_id=callback.message.chat.id,
+                                    text="Код исправления ошибок (ECC), определяет степень избыточности"
+                                         " данных. Чем выше избыточность данных, тем больше данных можно восстановить,"
+                                         " если QR-код поврежден (например, царапины на наклейке с QR-кодом или что-то"
+                                         " в этом роде).\nБолее высокий ECC приводит к сохранению большего количества "
+                                         "данных и, следовательно, к QR-коду с большим количеством пикселей данных"
+                                         " и большей матрицей данных. Самый низкий ECC является лучшим выбором для "
+                                         "общих целей — устаревшие считыватели QR-кодов являются более распространенной"
+                                         " проблемой, чем поврежденные QR-коды", reply_markup=get_keyboard_for_ecc())
+        await Settings.ecc.set()
+
+    elif callback.data == "color":
+        await bot.edit_message_text(message_id=callback.message.message_id, chat_id=callback.message.chat.id,
+                                    text="Код исправления ошибок (ECC), определяет степень избыточности"
+                                         " данных. Чем выше избыточность данных, тем больше данных можно восстановить,"
+                                         " если QR-код поврежден (например, царапины на наклейке с QR-кодом или что-то"
+                                         " в этом роде).\nБолее высокий ECC приводит к сохранению большего количества "
+                                         "данных и, следовательно, к QR-коду с большим количеством пикселей данных"
+                                         " и большей матрицей данных. Самый низкий ECC является лучшим выбором для "
+                                         "общих целей — устаревшие считыватели QR-кодов являются более распространенной"
+                                         " проблемой, чем поврежденные QR-коды", reply_markup=get_keyboard_for_color())
+        await Settings.color.set()
+    elif callback.data == "bgcolor":
+        await bot.edit_message_text(message_id=callback.message.message_id, chat_id=callback.message.chat.id,
+                                    text="Код исправления ошибок (ECC), определяет степень избыточности"
+                                         " данных. Чем выше избыточность данных, тем больше данных можно восстановить,"
+                                         " если QR-код поврежден (например, царапины на наклейке с QR-кодом или что-то"
+                                         " в этом роде).\nБолее высокий ECC приводит к сохранению большего количества "
+                                         "данных и, следовательно, к QR-коду с большим количеством пикселей данных"
+                                         " и большей матрицей данных. Самый низкий ECC является лучшим выбором для "
+                                         "общих целей — устаревшие считыватели QR-кодов являются более распространенной"
+                                         " проблемой, чем поврежденные QR-коды", reply_markup=get_keyboard_for_color())
+        await Settings.bgcolor.set()
+    elif callback.data == "done":
+        await callback.message.delete()
+        await state.finish()
 
 
 @dp.message_handler(lambda message: message.text.isalnum(), state=Settings.size)
 async def get_size(message: types.Message, state: FSMContext):
-    n = int(message.text)
-    if 10 <= n <= 1000:
-        update_settings(message, "size", n)
-        await message.delete()
+    await get_int(message, state, lambda x: 10 <= x <= 1000, "size")
 
-        async with state.proxy() as data:
-            await go_to_menu(data["menu"], state)
 
+@dp.message_handler(lambda message: message.text.isalnum(), state=Settings.margin)
+async def get_margin(message: types.Message, state: FSMContext):
+    await get_int(message, state, lambda x: 0 <= x <= 50, "margin")
+
+
+@dp.message_handler(lambda message: message.text.isalnum(), state=Settings.qzone)
+async def get_qzone(message: types.Message, state: FSMContext):
+    await get_int(message, state, lambda x: 0 <= x <= 100, "qzone")
+
+
+@dp.callback_query_handler(state=Settings.ecc)
+async def get_ecc(callback: types.CallbackQuery, state: FSMContext):
+    await get_callback(callback, state, "ecc")
+
+
+@dp.callback_query_handler(state=Settings.color)
+async def get_ecc(callback: types.CallbackQuery, state: FSMContext):
+    await get_callback(callback, state, "color")
+
+
+@dp.callback_query_handler(state=Settings.bgcolor)
+async def get_ecc(callback: types.CallbackQuery, state: FSMContext):
+    await get_callback(callback, state, "bgcolor")
 
 
 if __name__ == "__main__":
--- functions.py
+++ functions.py
@@ -1,12 +1,15 @@
 import sqlite3
 from aiogram import types
+from aiogram.dispatcher import FSMContext
+
+from bot import go_to_menu
 
 
 def create_settings(message: types.Message) -> None:
     with sqlite3.connect('db/data.db') as con:
         cursor = con.cursor()
         results = cursor.execute(
-            f"""INSERT INTO settings VALUES({message.from_user.id}, {200}, 'L', '(0,0,0)', '(255,255,255)', 0, 10)""").fetchall()
+            f"""INSERT INTO settings VALUES({message.from_user.id}, {200}, 'L', '0-0-0', '255-255-255', 0, 10)""").fetchall()
 
 
 def get_settings(message: types.Message) -> dict:
@@ -27,16 +30,44 @@ def get_settings(message: types.Message) -> dict:
     return settings_dict
 
 
-def update_settings(message: types.Message, settins_name: str, value) -> None:
+def update_settings(user_id: int, settins_name: str, value) -> None:
     with sqlite3.connect('db/data.db') as con:
+        print("start")
         cursor = con.cursor()
         try:
+            print("try")
             value = int(value)
-            cursor.execute(f"""UPDATE settings
-                                SET {settins_name} = {value}
-                                WHERE user_id = {message.from_user.id}""")
+
         except ValueError:
+            print("Except")
             cursor.execute(f"""UPDATE settings
-                                            SET {settins_name} = '{value}'
-                                            WHERE user_id = {message.from_user.id}""")
+                               SET {settins_name} = '{value}'
+                               WHERE user_id = {user_id}""")
+
+        else:
+            print("else")
+            cursor.execute(f"""UPDATE settings
+                               SET {settins_name} = {value}
+                               WHERE user_id = {user_id}""")
+
         con.commit()
+
+
+async def get_int(message: types.Message, state: FSMContext, borders, name: str):
+    n = int(message.text)
+    if borders(n):
+        update_settings(message.from_user.id, name, n)
+        await message.delete()
+
+        async with state.proxy() as data:
+            await go_to_menu(data["menu"], state)
+
+    else:
+        await message.delete()
+
+
+async def get_callback(callback: types.CallbackQuery, state: FSMContext, name):
+
+    update_settings(callback.from_user.id, name, callback.data)
+    async with state.proxy() as data:
+        await go_to_menu(data["menu"], state)
